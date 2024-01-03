ec854b5383563d9842423f5ca596fe32b0f2a501
egcutler <ericgcutler@gmail.com>
2024-01-03 14:41:25 -0600

    generategenerate_random_record_length() and table_generate() functions

--- generate_sample_tables.py
+++ generate_sample_tables.py
@@ -1,19 +1,31 @@
 import pandas as pd
 import random
 
+# Generate a random record limit for the data frame
+def generate_random_record_length(min = 500, max = 100000, seed_val = 50):
+      # Set the seed for reproducibility
+      random.seed(seed_val)
+      # Number of records
+      return random.randint(min, max)
 
-# Set the seed for reproducibility (optional)
-random.seed(50)
+# Generate a DataFrame with a random ID and Account Columns
+def table_generate(num_records):
+      data = {'ID_Record': [random.randint(1, num_records) for x in range(num_records)]}
+      for x in data['ID_Record']:
+            zero_count = len(str(num_records)) - len(str(x))
+            if zero_count == 0:
+                  data['Account'] = str(x)
+            else:
+                  zero_addon = '0' * zero_count
+                  data['Account'] = zero_addon + str(x)
+      return data
+#------------------------------------------------------------------------------     
 
-# Number of records
-num_records = 10000
-
-# Generate a DataFrame with a random ID column
-data = {'ID': [random.randint(1, 100000) for x in range(num_records)]}
-df = pd.DataFrame(data)
-
+num_records = generate_random_record_length()
+data = table_generate(num_records)
+print(pd.DataFrame(data))
 
 
 # Display the DataFrame
-print(df)
-#df.to_csv("testtest.csv", index=False)
+#print(df)
+#df.to_csv("data archive/test.csv", index=False)
