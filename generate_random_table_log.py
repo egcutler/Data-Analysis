44dc327b3dcaee8003e6635d1673a7182e18c24f
egcutler <ericgcutler@gmail.com>
2024-01-09 17:54:22 -0600

    log small

--- generate_random_table_log.py
+++ generate_random_table_log.py
@@ -28,6 +28,22 @@ def generate_TTT__field(num_records):
 
 def generate_table_TTT_build(dict, num_records):  
       pass
+      #dict['Log ID'] =
+      #dict['Time Stamp'] =
+      #dict['User ID'] =
+      #dict['Event Type'] =
+      #dict['Description'] =
+      #dict['IP Address'] =
+      #dict['Hostname']
+      #dict['Severity Level'] =
+      #dict['Status'] =
+      #dict['Reference ID'] =
+      #dict['Old Value'] =
+      #dict['New Value'] =
+      #dict['Error Code'] =
+      #dict['Error Message'] =
+      #dict['Module'] =
+      #dict['Source'] =
 
 def generate_table_TTT(min_rand_record_lim = 1, max_rand_record_lim = 100000):
       addr_num_records = gtsf.generate_random_record_length(min_rand_record_lim, max_rand_record_lim)
