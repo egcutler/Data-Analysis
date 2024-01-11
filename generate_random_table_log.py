ff10ad6e7c77f975ae5cf53d385808c752f342ae
egcutler <ericgcutler@gmail.com>
2024-01-11 16:28:16 -0600

    log dictionaries

--- generate_random_table_log.py
+++ generate_random_table_log.py
@@ -3,10 +3,116 @@ import random
 from datetime import datetime, timedelta
 import generate_tables_support_functions as gtsf
 
+#----------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------
+#----------              Log List for Support Functions                   ---------
+#----------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------
+
+# Log Event Type dictionary for random generator
+log_dict_event = {
+      "User Activity" : "Logged the user activity",
+      "Server Event" : "Logged the server activity"
+      "Data Change" : "Logged a change in data",
+      "File Change" : "Logged a change in a file",
+      "System Error" : "Logged a system error",
+      "Security Alert" : "Logged a potential security breach"
+}
+
+# Log Description dictionaries for random generator
+log_dict_data_change = {
+    "Create": "Adding new data or records to the system. This could involve creating a new user account, adding a new product to a catalog, or entering new transaction details.",
+    "Update": "Modifying existing data or records. This includes changes like updating user information, altering product prices, or revising transaction amounts.",
+    "Delete": "Removing data or records from the system. This might involve deleting a user account, removing a product from the catalog, or voiding a transaction.",
+    "Archive": "Moving data to an archival state rather than deleting it. This allows for the preservation of data for historical or compliance purposes while reducing its active footprint in the system.",
+    "Merge": "Combining multiple data records into a single record. This is often used in scenarios like deduplicating customer records or consolidating transaction details.",
+    "Split": "Separating a single data record into multiple records. This might be used in cases where a single transaction needs to be divided into multiple parts for accounting purposes.",
+    "Rollback": "Reverting data to a previous state or version. This is used in scenarios where recent changes need to be undone.",
+    "Import": "Bringing in data from external sources. This could involve bulk uploading of data, such as importing a list of contacts or product details.",
+    "Export": "Sending data out to external systems or files. This typically involves generating reports or datasets for analysis or sharing with external parties.",
+    "Data Cleansing": "Correcting or removing inaccurate records from a dataset. This ensures the quality and reliability of the data in the system."
+}
+
+log_dict_file_change = {
+    "File Upload": "Notes when a user uploads a file, including details like file type and size.",
+    "File Deletion": "Logs the removal of a file from the system, detailing the file name and deletion time.",
+    "File Rename": "Records the action of changing a file's name, noting the original and new names of the file.",
+    "Form Submission": "Logs when a user submits a form, such as a contact form, registration form, or a settings change form.",
+    "File Modification": "Tracks changes made to a file, including edits to the content, format changes, or metadata updates.",
+    "File Download": "Records when a user downloads a file, including the file name and time of download.",
+    "File Access": "Logs each instance of a file being accessed or opened by a user, noting the file name and access time.",
+    "File Move": "Tracks when a file is moved from one location to another, recording the original and new locations.",
+    "File Copy": "Notes when a user makes a copy of a file, including details of the source and destination locations.",
+    "File Permission Change": "Logs changes made to file permissions, detailing the file name and the updated permissions."
+}
+
+log_dict_security = {
+    "Unauthorized Access Attempt": "Logs attempts to access the system or data without proper authorization.",
+    "Password Change Attempt": "Records attempts to change a password, whether successful or not.",
+    "Firewall Alert": "Logs alerts triggered by the firewall, indicating potential security threats.",
+    "Virus Detection": "Records instances where a virus or malware is detected by the system's antivirus software.",
+    "Data Breach": "Logs incidents where sensitive data is accessed or exposed in an unauthorized manner.",
+    "Security Patch Application": "Tracks the application of security patches to the system or software.",
+    "Encryption Status Change": "Logs changes in the encryption status of data, such as enabling or disabling encryption.",
+    "User Role Change": "Records changes in user roles, especially changes that affect access privileges.",
+    "Security Policy Update": "Logs updates or changes to security policies within the system.",
+    "Two-factor Authentication Event": "Tracks events related to two-factor authentication, including setup, changes, and authentication attempts.",
+    "Security Scan": "Records the outcomes of security scans, detailing identified vulnerabilities or anomalies.",
+    "Suspicious Activity": "Logs activities flagged as suspicious, which could indicate a potential security threat."
+}
+
+log_dict_user_web = {
+    "Login Attempt": "Tracks each attempt a user makes to log in, whether successful or not.",
+    "Logout Attempt": "Tracks each attempt a user makes to log out, whether successful or not.",
+    "Comment Posting": "Tracks user comments on posts, articles, or other users' content.",
+    "Page Visit": "Records when a user visits a specific page or section of the application or website.",
+    "Search Query": "Logs the search terms a user enters in a search bar within the application.",
+    "Video View": "Tracks when a user views a video, including information on the video watched and duration of view.",
+    "Notification Interaction": "Records interactions with notifications, such as opening or dismissing them.",
+    "Profile Update": "Logs changes made to a user's profile, including photo updates, bio changes, etc.",
+    "File Upload": "Notes when a user uploads a file, including details like file type and size.",
+    "Item Purchase": "Records when a user makes a purchase, detailing the items bought and the transaction details.",
+    "Form Submission": "Logs when a user submits a form, such as a contact form, registration form, or a settings change form.",
+    "Social Media Share": "Logs when a user shares content from the application on social media platforms."
+}
+
+
+log_dict_user_server = {
+    "Server Start": "Logs when the server is started, including the timestamp and initial status.",
+    "Server Shutdown": "Records when the server is shut down, noting the time and reason for shutdown.",
+    "Service Start": "Tracks the initiation of a specific service on the server, including service name and start time.",
+    "Service Stop": "Logs when a service on the server is stopped, detailing the service name and stop time.",
+    "System Update": "Records updates made to the server's operating system or software packages.",
+    "Security Alert": "Logs security-related events, such as unauthorized access attempts or detected vulnerabilities.",
+    "Performance Metrics": "Tracks various performance metrics of the server, like CPU usage, memory usage, and disk space.",
+    "Backup Completion": "Records the completion of data backup processes, including time and status of the backup.",
+    "Error Reports": "Logs error events, with details about the error type, affected components, and timestamps.",
+    "Configuration Change": "Tracks changes made to server configurations, such as network settings or system parameters.",
+    "Network Activity": "Logs network-related activities, including incoming and outgoing traffic details.",
+    "Hardware Status": "Records the status of server hardware components, such as hard drives, memory modules, and CPUs."
+}
+
+log_dict_user_accout = {
+    "Login Attempt": "Tracks each attempt a user makes to log in, whether successful or not.",
+    "Logout Attempt": "Tracks each attempt a user makes to log out, whether successful or not.",
+    "Settings Change": "Tracks changes a user makes to their personal or application settings.",
+    "Connection Status": "Logs the status of a user's connection, including times of connection and disconnection.",
+    "Notification Interaction": "Records interactions with notifications, such as opening or dismissing them.",
+    "Password Change": "Logs when a user changes their password, including the timestamp of the change.",
+    "Account Creation": "Records the creation of a new user account, including details of the user and time of creation.",
+    "Account Deletion": "Tracks the deletion of a user account, noting the time and reason for deletion if available.",
+    "Role Assignment": "Logs changes to a user's role or permissions within the system.",
+    "Profile Update": "Records updates made to a user's profile information, such as name, email, or profile picture.",
+    "Two-factor Authentication Setup": "Logs the setup or changes to two-factor authentication settings for a user.",
+    "Session Timeout": "Records instances of user sessions timing out due to inactivity."
+}
+
+
+
 
 #----------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------
-#----------              Log Functions for Generator                ---------- 
+#----------              Log Functions for Generator                     ---------- 
 #----------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------
 
