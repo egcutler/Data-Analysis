21ef39140dcede3bae4b2d9f7805d7b4dc2ac115
egcutler <ericgcutler@gmail.com>
2024-01-17 16:59:43 -0600

    Threshold and Status updates

--- data_analysis_flag_identifications.py
+++ data_analysis_flag_identifications.py
@@ -1,4 +1,50 @@
 # ---------------
 import pandas as pd
 from pathlib import Path
+import pandas as pd
+import generate_tables_support_functions as gtsf
+import generate_sample_table_business as gstb
+import generate_sample_table_legal as gstl
+import generate_sample_table_address as gsta
+import generate_sample_table_employee as gste
+import generate_sample_table_tax as gstt
+import generate_sample_table_financial as gstf
+import generate_random_table_log as grtl
+import data_integrity_support_files as disf
+import data_integrity_data_checks as didc
+
+
+# ---------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------
+# --------------                    Prechecks                                                --------------
+# ---------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------
+
+path = (r'/root/workspace/github.com/egcutler/Data-Analysis/data archive')
+filename = 'business data'
+partial_name = 'business'
+filetype = '.csv'
+field_list = ['Account','Branch','External ID']
+
+precheck_folder = disf.FolderPreCheck(path)
+if precheck_folder.check_files_with_partial_name(partial_name, filetype):
+      precheck_file = disf.FilePreCheck(path, filename, filetype)
+      precheck_file.file_exist_precheck()
+      precheck_file.fields_exist_precheck(field_list)
+      id_lcase_check = precheck_file.check_fields_with_partialname("id")
+      id_ucase_check = precheck_file.check_fields_with_partialname("ID")
+      if id_lcase_check == False and id_ucase_check == False:
+            raise Exception("Programm terminated...no valid ID fields present")
+      df = pd.read_csv(f"{path}/{filename}{filetype}")
+
+# ---------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------
+# --------------                    Data Quality Checks                                      --------------
+# ---------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------
+#precheck_file.search_fields_with_partialname("ID")
+
+datacheck = didc.Data_Check(df)
+datacheck.id_unique_count_thresholds('External ID')
+datacheck.active_ids('External ID', "Business Status")
 
--- data_integrity_data_checks.py
+++ data_integrity_data_checks.py
@@ -62,40 +62,98 @@ file_types_pd = {
 
 class Data_Check:
 
-    def __init__(self, df):
-        """
-        Initialize the FilePostCheck object with a DataFrame.
-        - Parameter df: The DataFrame to be checked.
-        """
-        self.df = df
+      def __init__(self, df):
+            """
+            Initialize the FilePostCheck object with a DataFrame.
+            - Parameter df: The DataFrame to be checked.
+            """
+            self.df = df
 
-    def id_unique_count_thresholds(self, id_col, threshold_low_id=10, threshold_high_id=500000, threshold_low_row=10, threshold_high_row=500000):
-        """
-        Check the row count and unique ID count of the DataFrame.
-        Flags are raised if the counts are outside the specified thresholds.
-        - Parameter id_col: Column name of the ID in the DataFrame.
-        - Parameter threshold_low_id: Minimum threshold for distinct ID count (default 10).
-        - Parameter threshold_high_id: Maximum threshold for distinct ID count (default 500000).
-        - Parameter threshold_low_row: Minimum threshold for record (row) count (default 10).
-        - Parameter threshold_high_row: Maximum threshold for record (row) count (default 500000).
-        """
-        ids = self.df[id_col].tolist()
-        unique_ids = set(ids)
-        count_distinct_ids = len(unique_ids)
-        record_count = len(ids)
+      def id_unique_count_thresholds(self, id_col, threshold_low_id=10, threshold_high_id=500000, \
+                                     threshold_low_row=10, threshold_high_row=500000):
+            """
+            Check the row count and unique ID count of the DataFrame.
+            Flags are raised if the counts are outside the specified thresholds.
+            - Parameter id_col: Column name of the ID in the DataFrame.
+            - Parameter threshold_low_id: Minimum threshold for distinct ID count (default 10).
+            - Parameter threshold_high_id: Maximum threshold for distinct ID count (default 500000).
+            - Parameter threshold_low_row: Minimum threshold for record (row) count (default 10).
+            - Parameter threshold_high_row: Maximum threshold for record (row) count (default 500000).
+            """
+            ids = self.df[id_col].tolist()
+            unique_ids = set(ids)
+            count_distinct_ids = len(unique_ids)
+            record_count = len(ids)
+            flag_count = 0
 
-        # Check against low unique ID threshold
-        if count_distinct_ids < threshold_low_id:
-            print(f'Warning: Distinct ID count below minimum threshold ({threshold_low_id})!')
+            # Check against low unique ID threshold
+            if count_distinct_ids < threshold_low_id:
+                  print(f'Warning: Distinct ID count {count_distinct_ids} is below minimum threshold ({threshold_low_id})!')
+                  flag_count += 1
+            # Check against high unique ID threshold
+            if count_distinct_ids > threshold_high_id:
+                  print(f'Warning: Distinct ID count {count_distinct_ids} is above maximum threshold ({threshold_high_id})!')
+                  flag_count += 1
+            # Check against high row count threshold
+            if record_count > threshold_high_row:
+                  print(f'Warning: Record count {record_count} is above maximum threshold ({threshold_high_row})!')
+                  flag_count += 1
+            # Check against low row count threshold
+            if record_count < threshold_low_row:
+                  print(f'Warning: Record count {record_count} is below minimum threshold ({threshold_low_row})!')
+                  flag_count += 1
+            # Passed/Failed message    
+            if flag_count == 0:
+                  print("...ID threshold check: Passed")
+            else:
+                  print(f"...{flag_count} ID threshold warnings triggered")
+                  print(f"   Distinct ID count: {count_distinct_ids}")
+                  print(f"   Record count: {record_count}")
 
-        # Check against high unique ID threshold
-        if count_distinct_ids > threshold_high_id:
-            print(f'Warning: Distinct ID count above maximum threshold ({threshold_high_id})!')
+      def active_ids(self, id_col, status_col, min_act_threshold=10, max_cld_threshold = 500000, \
+                     act_ratio_threshold=0.5, cld_ratio_threshold=0.1):
+            """
+            Check the ACTIVE/CLOSED account statistics in the DataFrame.
+            Flags are raised if the number of active accounts is below a specified threshold
+            or if the ratio of ACTIVE to CLOSED accounts falls below another specified threshold.
+            - Parameter id_col: Column name of the ID in the DataFrame.
+            - Parameter status_col: Column name of the account status (ACTIVE/CLOSED).
+            - Parameter act_threshold: Minimum number of active accounts threshold (default 10).
+            - Parameter act_ratio_threshold: Minimum ratio of ACTIVE to CLOSED accounts (default 1).
+            """
+            # Creating a DataFrame with only ID and Active Columns
+            df_ids = self.df[[id_col, status_col]]
 
-        # Check against high row count threshold
-        if record_count > threshold_high_row:
-            print(f'Warning: Record count above maximum threshold ({threshold_high_row})!')
+            # Count Active and Closed IDs
+            count_act_ids = df_ids[status_col].str.upper().eq('ACTIVE').sum()
+            count_cld_ids = df_ids[status_col].str.upper().eq('CLOSED').sum()
+            count_id_tot = len(df_ids[status_col])
 
-        # Check against low row count threshold
-        if record_count < threshold_low_row:
-            print(f'Warning: Record count below minimum threshold ({threshold_low_row})!')
+            # Calculate the ratio of ACTIVE to CLOSED accounts
+            act_ratio = count_act_ids / count_id_tot if count_id_tot != 0 else float('inf')
+            cld_ratio = count_act_ids / count_id_tot if count_id_tot != 0 else float('inf')
+
+            flag_count = 0
+            # Check against active account threshold
+            if count_act_ids < min_act_threshold:
+                  print(f'FlWarningag: Active IDs below minimum threshold ({min_act_threshold})!')
+                  flag_count += 1
+            if count_cld_ids > max_cld_threshold:
+                  print(f'Warning: Closed IDs above maximum threshold ({max_cld_threshold})!')
+                  flag_count += 1
+            # Check against active to total ratio threshold
+            elif act_ratio < act_ratio_threshold:
+                  print(f'Warning: Ratio of Active to Total IDs ({act_ratio:.2f}) is below threshold ({act_ratio_threshold})!')
+                  flag_count += 1
+            # Check against active to total ratio threshold
+            elif cld_ratio < cld_ratio_threshold:
+                  print(f'Warning: Ratio of Active to Total IDs ({act_ratio:.2f}) is below threshold ({act_ratio_threshold})!')
+                  flag_count += 1
+            # Passed/Failed message    
+            if flag_count == 0:
+                  print("...Active/Closed threshold checks: Passed")
+            else:
+                  print(f"...{flag_count} Active/Closed threshold warnings triggered")
+                  print(f"   Active status count: {count_act_ids}")
+                  print(f"   Closed status count: {count_cld_ids}")
+                  print(f"   Total status count: {count_id_tot}")
