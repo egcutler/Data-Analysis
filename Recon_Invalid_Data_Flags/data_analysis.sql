6ab86132a0555c9fd45f308f68d958d7a65d657e
egcutler <ericgcutler@gmail.com>
2024-02-03 18:49:56 -0600

    tableau

--- Recon_Invalid_Data_Flags/data_analysis.sql
+++ Recon_Invalid_Data_Flags/data_analysis.sql
@@ -1,4 +1,5 @@
-WITH CID_ACCOUNT AS (
+WITH 
+CID_ACCOUNT AS (
 	SELECT DISTINCT
 	BA.External_ID,
 	BA.Company_Name,
@@ -24,19 +25,54 @@ WITH CID_ACCOUNT AS (
 	ON BA.External_ID = LE.Bus_External_ID
 	WHERE BA.Business_Status = 'ACTIVE'
 	AND LE.Legal_Status = 'ACTIVE'
-	)
+	),
+
+ADDRESS AS (
+	SELECT DISTINCT
+	AD.Address_ID,
+	AD.Address_Street,
+	AD.City,
+	AD.State,
+	AD.Zip_Code,
+	AD.Registered_Country,
+	AD.Flag_Street_Address,
+	AD.Bus_External_ID
+
+	FROM dbo.[Address Data With Flags] AD
+),
+
+EMPLOYEE AS (
+	SELECT DISTINCT
+	EMP.Employee_ID,
+	EMP.Emp_First_Name,
+	EMP.Emp_Last_Name,
+	EMP.Employee_Status,
+	EMP.Employee_Email,
+	EMP.Job_Title,
+	EMP.Flag_Email
+
+	FROM dbo.[Employee Data With Flags] EMP
+)
 
 SELECT DISTINCT
-CID.*,
-AD.Address_ID,
-AD.Address_Street,
-AD.City,
-AD.State,
-AD.Zip_Code,
-AD.Registered_Country,
-AD.Flag_Street_Address
+C.*,
+A.Address_ID,
+A.Address_Street,
+A.City,
+A.State,
+A.Zip_Code,
+A.Registered_Country,
+A.Flag_Street_Address,
+A.Bus_External_ID,
+E.*,
+GETDATE() AS 'Time Stamp'
 
-FROM CID_ACCOUNT CID
-LEFT JOIN [Address Data With Flags] AD
-ON CID.External_ID = AD.Bus_External_ID
+FROM CID_ACCOUNT C
+LEFT JOIN ADDRESS A
+ON C.External_ID = A.Bus_External_ID
+LEFT JOIN dbo.[Employee System] ES
+ON C.Legal_Account = ES.Legal_Account
+LEFT JOIN EMPLOYEE E
+ON ES.Employee_ID = E.Employee_ID
+
 GO
